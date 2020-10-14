b4bacf345ec5ea2d0d9e882c586ea54f9bfa8806
Jason Adams <jason.the.adams@gmail.com>
2020-10-14 16:41:49 -0700

    feature: add a .releaseignore file

--- .github/prerelease.sh
+++ .github/prerelease.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 apt-get update
-apt-get install php libapache2-mod-php zip unzip -y
+apt-get install php libapache2-mod-php zip -y
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '795f976fe0ebd8b75f26a6dd68f78fd3453ce79f32ecb33e7fd087d39bfeb978342fb73ac986cd4f54edd0dc902601dc') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
@@ -12,8 +12,7 @@ composer install
 
 echo "Remove stuff"
 
-rm -rf .git
-rm -rf .github
+while IFS= read -r file ; do rm -- "$file" ; done < .releaseignore
 
 echo "Zipping stuff"
 
